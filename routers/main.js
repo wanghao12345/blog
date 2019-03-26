67361a33ac32fa6fb464701030db368753220cbb
winner <1750110604@qq.com>
2019-03-26 19:03:39 +0800

    分模块

--- routers/main.js
+++ routers/main.js
@@ -1,13 +1,19 @@
 // 引入 express 模块
 var express = require('express');
 
+// 引入 category model模块
+var Category = require('../models/Category')
+
 // 使用express的Router()路由方法
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-  res.render('main/index', {
-    userInfo: req.userInfo
-  })
+  Category.find().then(function (categories) {
+    res.render('main/index', {
+      userInfo: req.userInfo,
+      categories: categories
+    })
+  });
 });
 
 module.exports = router;
