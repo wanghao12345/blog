10707530ce6411af0c9bb31c21270c1f6cd9b976
winner <1750110604@qq.com>
2019-03-29 18:39:21 +0800

    文章评论

---
+++ schemas/comments.js
@@ -0,0 +1,28 @@
+// 引入 mongoose 模块
+var mongoose = require('mongoose');
+
+// 评论的表结构
+module.exports = new mongoose.Schema({
+
+  // 关联字段 - 内容的id
+  contents: {
+    // 类型
+    type: mongoose.Schema.Types.ObjectId,
+    // 引用
+    ref: 'Content'
+  },
+
+  // 父级评论id
+  fid: {
+    // 类型
+    type: mongoose.Schema.Types.ObjectId,
+    // 引用
+    ref: 'Comment'
+  },
+
+  // 评论内容
+  describe: String,
+
+  // 评论时间
+  createTime: Date
+});
